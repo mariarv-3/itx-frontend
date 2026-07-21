a2a982c96651a35503ec6023fa06e60e09121d0c
María Rey <maria.rey@n3team.com>
2026-07-21 11:45:45 +0200

    updated doc

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   presets: [
-      "@babel/preset-env",
+    "@babel/preset-env",
+    [
       "@babel/preset-react",
-      "@babel/preset-typescript",
+      {
+        runtime: "automatic",
+      },
+    ],
+    "@babel/preset-typescript",
   ],
 };
